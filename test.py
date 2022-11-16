8d287c5fa59f612cdb4d1d63959ecff607e09e49
lucaspons9 <100160083+lucaspons9@users.noreply.github.com>
2022-11-17 00:47:10 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -5,54 +5,59 @@ import pandas as pd
 
 def load():
     #acceso al servicio web del bicing
-    r = requests.get('http://wservice.viabicing.cat/v2/stations')
+    r = requests.get('https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_status')
     bicingJson = r.json()
 
-    dic = {'Casa': 332,
-    'Dalmases': 329,
-    #'upc_abajo': 422,
-    #'upc_arriba': 429
-    'Esade': 302,
-    'Pedralbes': 303,
-    'Upper': 304}
+    dic = {'casa': 332,
+    'dalmases': 329,
+    'upc_abajo': 422,
+    'esade': 302,
+    'cuartel': 429,
+    'pedralbes': 303,
+    'upper': 304}
 
     dic_real = {}
-
-    b_casa = bicingJson['stations']
+    b_casa = bicingJson['data']['stations']
     for key in dic.keys():
         for i in range(len(b_casa)):
-            if (b_casa[i]['id'] == str(dic[key])):
+            if (b_casa[i]['station_id'] == dic[key]):
                 dic_real[key] = i
 
     bicis_sitios = {}
     for key in dic_real.keys():
-        bicis, sitios = bicingJson['stations'][dic_real[key]]['bikes'],bicingJson['stations'][dic_real[key]]['slots']
-        bicis_sitios[key] = [bicis, sitios]
+        bicis_sitios[key] = [bicingJson['data']['stations'][dic_real[key]]['num_bikes_available_types']['mechanical'],
+                             bicingJson['data']['stations'][dic_real[key]]['num_bikes_available_types']['ebike'],
+                             bicingJson['data']['stations'][dic_real[key]]['num_docks_available']]
 
-    df = pd.DataFrame(columns = ['Estación','Bicis', 'Sitios'])
+    df = pd.DataFrame(columns = ['Estación','Mecánicas', 'Eléctricas', 'Sitios Libres'])
     for key in bicis_sitios.keys():
-        # df.append({'Estación': key, 'Bicis': bicis_sitios[key][0], 'Sitios': bicis_sitios[key][1]}, ignore_index=True)
-        df.loc[len(df)] = [key, bicis_sitios[key][0], bicis_sitios[key][1]]
+        df.loc[len(df)] = [key, bicis_sitios[key][0], bicis_sitios[key][1], bicis_sitios[key][2]]
 
     return df
 
 
 if st.button('Load data'):
     df = load()
-    if df.iloc[0]['Bicis'] != '0':
-        est_ini = 'Casa'
-    elif df.iloc[1]['Bicis'] != '0':
-        est_ini = 'Dalmases'
-    else:
-        est_ini = None
-    st.markdown(f'### Coger en *{est_ini}*')
-    if df.iloc[2]['Sitios'] != '0':
-        est_fin = df.iloc[2]['Estación']
-    elif df.iloc[3]['Sitios'] != '0':
-        est_fin = df.iloc[3]['Estación']
-    elif df.iloc[4]['Sitios'] != '0':
-        est_fin = df.iloc[4]['Estación']
-    else:
-        est_fin = None
-    st.markdown(f'### Dejar en *{est_fin}*')
+    m = df.iloc[0]['Mecánicas']
+    e = df.iloc[0]['Eléctricas']
+    st.markdown(f'##### **Casa** \n + Mecánicas ➡️ {m} \n + Eléctricas ➡️ {e}')
+    st.markdown('\n')
+
+    first = -1
+    second = -1
+    for i in range(6, 1, -1):
+        if df.iloc[i]['Sitios Libres'] != 0:
+            second = first
+            first = i
+
+    if first != -1:
+        est_fin_first = df.iloc[first]['Estación']
+        sit_first = df.iloc[first]['Sitios Libres']
+        st.markdown(f'##### 1. Dejar en *{est_fin_first}* ({sit_first} Sitios libres)')
+
+    if second != -1:
+        est_fin_second = df.iloc[second]['Estación']
+        sit_second = df.iloc[second]['Sitios Libres']
+        st.markdown(f'##### 2. Dejar en *{est_fin_second}* ({sit_second} Sitios libres)')
+    st.markdown('___')
     st.dataframe(df, width = 1000)
